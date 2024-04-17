c4b75ade428129a8a8ba03db4be8eb94a7ad6841
Vinicius Silva Souza <viniciusvss120@gmail.com>
2024-04-17 10:22:35 -0400

    Fazendo o test do refresh token

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import fastify from "fastify";
+import fastifyCookie from "@fastify/cookie";
 
 import { usersRoutes } from "./http/controller/users/routes";
 import { ZodError } from "zod";
@@ -10,8 +11,16 @@ import { checkInsRoutes } from "./http/controller/check-ins/routes";
 export const app = fastify();
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: "refreshToken",
+    signed: false,
+  },
+  sign: {
+    expiresIn: "10m",
+  },
 });
 
+app.register(fastifyCookie);
 // Aqui estamos registrando as rotas respectivas a cada parte da aplicação
 app.register(usersRoutes);
 app.register(gymsRoutes);
